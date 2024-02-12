ff168ecd497305c8d65cac950db6193c55c06a20
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-13 04:43:56 +0700

    Add users post function

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,9 +1,81 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.users_get = asyncHandler(async (req, res, next) => {
   res.send("GET request for all users data");
 });
 
-exports.users_post = asyncHandler(async (req, res, next) => {
-  res.send("POST request to users");
-});
+exports.users_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("last_name").trim().escape(),
+  body("email", "Email is not valid").trim().isEmail().escape(),
+  body("username")
+    .trim()
+    .isLength({ min: 4 })
+    .escape()
+    .withMessage("Username must be at least 4 characters")
+    .not()
+    .contains(" ")
+    .withMessage("Username cannot contain spaces")
+    .matches(/^[A-Za-z0-9_]+$/)
+    .withMessage(
+      "Username can only contain alphanumeric characters and underscores"
+    )
+    .custom(async (value) => {
+      const existingUser = await User.findOne({ username: value }).exec();
+      if (existingUser) {
+        throw new Error("Username has already been taken");
+      }
+      return true;
+    }),
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .escape()
+    .withMessage("Password must be at least 8 character")
+    .isAlphanumeric()
+    .withMessage("Password must be alphanumeric"),
+  body(
+    "confirm_password",
+    "The confirmation password does not match the original password"
+  )
+    .trim()
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        next(err);
+      } else {
+        const user = new User({
+          firstName: req.body.first_name,
+          lastName: req.body.last_name,
+          email: req.body.email,
+          username: req.body.username,
+          password: hashedPassword,
+        });
+
+        if (!errors.isEmpty()) {
+          res.status(400).json(errors);
+          return;
+        } else {
+          await user.save();
+          res.json(user);
+        }
+      }
+    });
+  }),
+];
