49a3ea42b95931fd7134c7a4fb6e135ca9b06394
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-13 04:43:39 +0700

    Add mongodb connect functions

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const commentRouter = require("./routes/comment_router");
 
 const app = express();
 
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
