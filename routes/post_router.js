c560e45c3ef3c684b6312f42b01006b5c9259158
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-13 17:41:11 +0700

    Add middleware to post route

--- routes/post_router.js
+++ routes/post_router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const post_controller = require("../controllers/post_controller");
+const verifyToken = require("../middlewares/authToken");
 
 router.get("/", post_controller.posts_get);
 
-router.post("/", post_controller.posts_post);
+router.post("/", verifyToken, post_controller.posts_post);
 
 module.exports = router;
