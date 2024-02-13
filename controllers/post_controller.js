646b3a2f32da47fc5c59104e552247a4777a1c29
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-13 17:40:56 +0700

    Add post method function

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -6,6 +6,30 @@ exports.posts_get = asyncHandler(async (req, res, next) => {
   res.send("GET request for all posts");
 });
 
-exports.posts_post = asyncHandler(async (req, res, next) => {
-  res.send("POST request to posts");
-});
+exports.posts_post = [
+  body("title", "Title must be at least 3 character")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("text", "Text must be at least 3 character")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty) {
+      res.status(400).json({ errors });
+    } else {
+      const post = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        user: req.user._id,
+      });
+
+      await post.save();
+      res.json({ message: "Added new post" });
+    }
+  }),
+];
