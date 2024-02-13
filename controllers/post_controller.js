35437936ea7ca2d90f17ca35639633782d3ce84d
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-14 06:48:19 +0700

    Add post update function

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -44,3 +44,37 @@ exports.post_single_get = asyncHandler(async (req, res, next) => {
 
   res.json(post);
 });
+
+exports.post_single_update = [
+  body("title", "Title must be at least 3 character")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("text", "Text must be at least 3 character")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const oldPost = await Post.findById(req.params.post_id);
+    if (!oldPost) res.sendStatus(400);
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty) {
+      res.status(400).json({ errors });
+    } else {
+      const post = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        user: req.user._id,
+        created_at: oldPost.created_at,
+        modified_at: Date.now(),
+        _id: req.params.post_id,
+      });
+
+      await Post.findByIdAndUpdate(req.params.post_id, post);
+      res.json(post);
+    }
+  }),
+];
