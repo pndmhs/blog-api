7f3ae99a50401104ab5444e7390574400f0cb438
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-13 16:42:41 +0700

    Fix timestamp type from "date" to "Date"

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   title: { type: String, require: true, minLength: 3 },
   text: { type: String, require: true, minLength: 3 },
-  timestamp: { type: date, default: Date.now() },
+  timestamp: { type: Date, default: Date.now() },
   user: { type: Schema.ObjectId, ref: "User" },
 });
 
