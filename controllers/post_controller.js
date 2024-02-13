9c32dc45230b681152c1560fece6dc7fbcf7e4ad
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-13 18:08:52 +0700

    Change posts get function

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -3,7 +3,10 @@ const { body, validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.posts_get = asyncHandler(async (req, res, next) => {
-  res.send("GET request for all posts");
+  const allPosts = await Post.find()
+    .populate("user", "username fullName")
+    .exec();
+  res.json({ posts: allPosts });
 });
 
 exports.posts_post = [
