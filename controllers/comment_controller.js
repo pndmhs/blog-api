7db59c1ab3bf1f5cb48b7667eeac8af9ba7213c1
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-13 22:06:30 +0700

    Change comments get and post functions

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,9 +1,34 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const Comment = require("../models/comment");
 
 exports.comments_get = asyncHandler(async (req, res, next) => {
-  res.send("GET request for all comments from post id: " + req.params.post_id);
+  const allComments = await Comment.find({ post: req.params.post_id });
+
+  res.json({ comments: allComments });
 });
 
-exports.comments_post = asyncHandler(async (req, res, next) => {
-  res.send("POST request to comments");
-});
+exports.comments_post = [
+  body("text", "Comment must be at least 3 character")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("username").trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty) {
+      res.status(400).json({ errors });
+    } else {
+      const comment = new Comment({
+        text: req.body.text,
+        username: req.body.username,
+        post: req.params.post_id,
+      });
+
+      await comment.save();
+      res.json({ message: "Added new comment" });
+    }
+  }),
+];
