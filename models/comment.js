1f43d7810ee9fc4230a667bf64a08291d8b63bfe
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-14 07:53:20 +0700

    Change username field schema

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
   text: { type: String, require: true, minLength: 3 },
   timestamp: { type: Date, default: Date.now() },
-  username: { type: String, minLength: 3 },
+  username: { type: String, default: null },
   post: { type: Schema.ObjectId, ref: "Post" },
 });
 
