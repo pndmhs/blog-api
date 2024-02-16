d7b30377205f6449aa9ff729ac105ab240a23b1f
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-17 05:30:11 +0700

    Delete user get router

--- routes/user_router.js
+++ routes/user_router.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const user_controller = require("../controllers/user_controller");
 
-router.get("/", user_controller.users_get);
-
 router.post("/", user_controller.users_post);
 
 router.post("/auth", user_controller.user_auth);
