f9abae889ea1ec35acea28ccaa3bcc77aa5c0f05
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-22 08:05:08 +0700

    Add helmet middleware and rate limit functions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
 
 require("dotenv").config();
 
@@ -9,6 +10,16 @@ const postRouter = require("./routes/post_router");
 const commentRouter = require("./routes/comment_router");
 
 const app = express();
+
+const RateLimit = require("express-rate-limit");
+
+const limiter = RateLimit({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 100,
+  message: "You have exceeded the 100 requests in 24 hrs limit!",
+});
+
+app.use(limiter);
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -23,6 +34,7 @@ async function main() {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
+app.use(helmet());
 app.use(cors());
 
 app.use("/users", userRouter);
