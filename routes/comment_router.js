bded6100fb1d81c16c350780f60de5de0b85b7c1
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-22 13:53:11 +0700

    Add delete router with verifyToken middleware

--- routes/comment_router.js
+++ routes/comment_router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const comment_controller = require("../controllers/comment_controller");
+const verifyToken = require("../middlewares/authToken");
 
 router.get("/", comment_controller.comments_get);
 
 router.post("/", comment_controller.comments_post);
+
+router.delete("/:comment_id", verifyToken, comment_controller.comment_delete);
 
 module.exports = router;
